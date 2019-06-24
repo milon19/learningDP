32bf59081aafc497696b90620ecdb44dc6b1a04c
Milon Mahato <33964827+milon19@users.noreply.github.com>
2019-06-24 12:29:05 +0600

    Create 5_LIS_length_O(n^2).cpp

---
+++ 5_LIS_length_O(n^2).cpp
@@ -0,0 +1,40 @@
+/*
+
+    LIS from Jane Alam Jaan.
+    Length of LIS. O(n^2)
+*/
+
+#include<iostream>
+#include<cstdio>
+#include<climits>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    int arr[n], lis[n];
+    for(int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+        lis[i] = 1;
+    }
+    for(int i = 0; i < n; i++)
+    {
+        for(int j = i+1; j < n; j++)
+        {
+            if(arr[i] < arr[j])
+            {
+                if(lis[j] < lis[i]+1)
+                    lis[j] = lis[i] + +1;
+            }
+        }
+    }
+    int mx = INT_MIN;
+    for(int i = 0; i < n; i++)
+        mx = max(mx, lis[i]);
+    cout << mx << endl;
+
+    return 0;
+}
